23be33317d69c77b20544770a0fc21e255b6563e
Juan López Rubio <j.lopez.r@gmail.com>
2023-01-27 18:17:44 +0100

    Activity2 returns to Activity1

--- app/src/main/java/dsa/app1/Activity1.java
+++ app/src/main/java/dsa/app1/Activity1.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Activity1 extends AppCompatActivity {
+
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+        result = findViewById(R.id.result);
+        result.setText("Press Ok to launch Activity2");
+
     }
 
     public void onClick(View view) {
--- app/src/main/java/dsa/app1/Activity2.java
+++ app/src/main/java/dsa/app1/Activity2.java
@@ -1,19 +1,25 @@
 package dsa.app1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity2 extends AppCompatActivity {
 
+    TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        result = findViewById(R.id.result);
+        result.setText("Press Ok to return to Activity1");
     }
 
-    protected void btClick(View v) {
-
+    public void onClick(View view) {
+        finish();
     }
 }
